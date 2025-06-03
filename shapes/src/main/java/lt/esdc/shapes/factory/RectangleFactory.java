0bc8a74f8f4ffd82e3aee7e0a2acfe3f017b8202
Aidos <107695458+aidosatai@users.noreply.github.com>
2025-06-03 20:42:19 +0500

    Refactor RectangleFactory to accept Point arguments

--- shapes/src/main/java/lt/esdc/shapes/factory/RectangleFactory.java
+++ shapes/src/main/java/lt/esdc/shapes/factory/RectangleFactory.java
@@ -9,45 +9,30 @@ import lt.esdc.shapes.validator.RectangleValidator;
 import lt.esdc.shapes.validator.ValidationResult;
 
 public class RectangleFactory extends ShapeFactory {
-    private static final int REQUIRED_PARAMETERS_COUNT = 8; 
     private final RectangleValidator validator;
 
     public RectangleFactory(RectangleValidator validator) {
         this.validator = validator;
     }
 
-    public Shape createShape(String id, String... parameters) throws InvalidShapeException {
-        if (parameters == null || parameters.length != REQUIRED_PARAMETERS_COUNT) {
-            throw new InvalidShapeException("Invalid number of parameters for Rectangle creation");
+    public Shape createShape(String id,
+                             Point point1,
+                             Point point2,
+                             Point point3,
+                             Point point4) throws InvalidShapeException {
+        if (point1 == null || point2 == null || point3 == null || point4 == null) {
+            throw new InvalidShapeException("All points must be provided for Rectangle creation");
         }
 
-        try {
-            double x1 = Double.parseDouble(parameters[0]);
-            double y1 = Double.parseDouble(parameters[1]);
-            double x2 = Double.parseDouble(parameters[2]);
-            double y2 = Double.parseDouble(parameters[3]);
-            double x3 = Double.parseDouble(parameters[4]);
-            double y3 = Double.parseDouble(parameters[5]);
-            double x4 = Double.parseDouble(parameters[6]);
-            double y4 = Double.parseDouble(parameters[7]);
+        String rectangleId = (id != null) ? id : IDGenerator.generateId();
 
-            Point point1 = new Point(x1, y1);
-            Point point2 = new Point(x2, y2);
-            Point point3 = new Point(x3, y3);
-            Point point4 = new Point(x4, y4);
-            
-            String rectangleId = (id != null) ? id : IDGenerator.generateId();
+        Rectangle rectangle = new Rectangle(rectangleId, point1, point2, point3, point4);
 
-            Rectangle rectangle = new Rectangle(rectangleId, point1, point2, point3, point4);
-            
-            ValidationResult validationResult = validator.validate(rectangle);
-            if (!validationResult.isValid()) {
-                throw new InvalidShapeException("Invalid rectangle: " + validationResult.getErrorMessage());
-            }
-            
-            return rectangle;
-        } catch (NumberFormatException e) {
-            throw new InvalidShapeException("Invalid parameter format for Rectangle creation", e);
+        ValidationResult validationResult = validator.validate(rectangle);
+        if (!validationResult.isValid()) {
+            throw new InvalidShapeException("Invalid rectangle: " + validationResult.getErrorMessage());
         }
+
+        return rectangle;
     }
 }
